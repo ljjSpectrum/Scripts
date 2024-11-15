620b791d49010e4e0b2ffb10f07dff57971140f3
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2024-11-15 16:01:51 -0600

    Update WinTrustVerify.bat

--- WinTrustVerify.bat
+++ WinTrustVerify.bat
@@ -1,2 +1,2 @@
-REG ADD "HKLM\SOFTWARE\Software\Microsoft\Cryptography\Wintrust\Config" /v "EnableCertPaddingCheck" /t REG_DWORD /d 1 /f
-REG ADD "HKLM\SOFTWARE\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config" /v "EnableCertPaddingCheck" /t REG_DWORD /d 1 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Cryptography\Wintrust\Config" /v "EnableCertPaddingCheck" /t REG_DWORD /d 1 /f
+REG ADD "HKLM\SOFTWARE\Wow6432Node\Microsoft\Cryptography\Wintrust\Config" /v "EnableCertPaddingCheck" /t REG_DWORD /d 1 /f
