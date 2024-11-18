03577568f2b49064d80feeab90f266c2496c2c8c
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2024-11-18 09:41:15 -0600

    Update WindowsCore.bat

--- WindowsCore.bat
+++ WindowsCore.bat
@@ -1,1 +1,1 @@
-C:\%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\winget.exe upgrade --all
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\winget.exe upgrade --all --silent --accept-package-agreements --accept-source-agreements
