1758542fc74301f583f779ba0073cb5cb017ca81
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2024-11-18 09:54:48 -0600

    Update windowsCore.ps1

--- windowsCore.ps1
+++ windowsCore.ps1
@@ -1,3 +1,2 @@
 # PowerShell script to run winget upgrade --all
-Start-Process -FilePath "winget" -ArgumentList "upgrade --all" -Verb RunAs -Wait
-
+Start-Process -FilePath "%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\winget.exe" -ArgumentList "upgrade --all --silent --accept-package-agreements --accept-source-agreements" -Verb RunAs -Wait
