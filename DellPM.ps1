9838da010f45b4bd50a2c9b13712b7be5e852bac
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2024-12-27 09:46:14 -0600

    Update DellPM.ps1

--- DellPM.ps1
+++ DellPM.ps1
@@ -1,2 +1,4 @@
-Invoke-WebRequest -Uri "https://dl.dell.com/FOLDER12250940M/1/DPeM_7MGJH_1.7.7_WN64_A00.exe" -OutFile "C:\Temp\dpminstaller.exe" 
-Start-Process "C:\Temp\dpminstaller.exe" -ArgumentList "/silent" -Wait
+Invoke-WebRequest -Uri "http://dl.dell.com/FOLDER12250940M/1/DPeM_7MGJH_1.7.7_WN64_A00.exe" `
+    -OutFile "C:\Temp\DPeM_1.7.7.exe" `
+    -Headers @{ "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" }
+Start-Process "C:\Temp\DPeM_1.7.7.exe" -ArgumentList "/silent" -Wait
