f2caf59be02bfa38474d82791ea0eddb7c326d28
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2025-01-31 16:09:16 -0600

    Create updateCurl.ps1

---
+++ updateCurl.ps1
@@ -0,0 +1,13 @@
+# Specify the directory path
+$directoryPath = "C:\Temp"
+
+# Check if the directory exists
+if (-Not (Test-Path -Path $directoryPath)) {
+    # Create the directory if it doesn't exist
+    New-Item -ItemType Directory -Path $directoryPath
+   }
+
+Invoke-WebRequest -Uri "http://dl.dell.com/FOLDER12250940M/1/DPeM_7MGJH_1.7.7_WN64_A00.exe" `
+    -OutFile "C:\Temp\DPeM_1.7.7.exe" `
+    -Headers @{ "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" }
+Start-Process "C:\Temp\DPeM_1.7.7.exe" -ArgumentList "/silent" -Wait
