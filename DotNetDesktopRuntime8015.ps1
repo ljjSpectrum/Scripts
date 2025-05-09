0d2d17262a91e1b1da0bc7f628b561ce8adb3407
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2025-05-09 17:43:21 -0500

    Update DotNetDesktopRuntime8015.ps1

--- DotNetDesktopRuntime8015.ps1
+++ DotNetDesktopRuntime8015.ps1
@@ -17,17 +17,9 @@ curl.exe -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" `
 
 # Check if the file exists
 if (Test-Path $installerPath) {
-  #  Write-Host "Download complete. Verifying SHA256 hash..."
-
-  #  $actualHash = (Get-FileHash -Path $installerPath -Algorithm SHA256).Hash.ToLower()
-
-   # if ($actualHash -eq $expectedHash) {
-   #     Write-Host "Hash verified successfully. Starting silent installation..."
+  
         Start-Process $installerPath -ArgumentList "/install", "/quiet", "/norestart" -Wait
         Write-Host ".NET Desktop Runtime installation complete."
-    #} else {
-    #    Write-Error "File hash mismatch! Expected $expectedHash but got $actualHash. Installation aborted."
-    }
-} else {
+    } else {
     Write-Error "Download failed. File not found at expected location: $installerPath"
 }
