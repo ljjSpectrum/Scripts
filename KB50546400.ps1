d3da6a82106c7a6411c7b8397f896b028e08c13f
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2025-06-04 15:58:49 -0500

    Update KB50546400.ps1

--- KB50546400.ps1
+++ KB50546400.ps1
@@ -1,6 +1,12 @@
 # Set URL or network path to the .cab file
 $updateUrl = "https://github.com/ljjSpectrum/Scripts/blob/main/windows10.0-kb5059607-x64_e3d41d6b69501d1851fb1afd6562859151f539af.cab"
-$localPath = "$env:TEMP\KB5046400.cab"
+$localPath = "C:\TEMP\KB5046400.cab"
+
+# Ensure the directory exists
+if (-not (Test-Path $localPath)) 
+{
+    New-Item -ItemType Directory -Path $localPath | Out-Null
+}
 
 # Download the file
 Invoke-WebRequest -Uri $updateUrl -OutFile $localPath
