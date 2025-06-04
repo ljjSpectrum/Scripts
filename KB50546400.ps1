b5852256a29856d1a85d9f0b1aa26760409e9f1a
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2025-06-04 15:41:05 -0500

    Create KB50546400.ps1

---
+++ KB50546400.ps1
@@ -0,0 +1,12 @@
+# Set URL or network path to the .cab file
+$updateUrl = "https://github.com/ljjSpectrum/Scripts/blob/main/windows10.0-kb5059607-x64_e3d41d6b69501d1851fb1afd6562859151f539af.cab"
+$localPath = "$env:TEMP\KB5046400.cab"
+
+# Download the file
+Invoke-WebRequest -Uri $updateUrl -OutFile $localPath
+
+# Install the update
+Start-Process "dism.exe" -ArgumentList "/Online", "/Add-Package", "/PackagePath:$localPath", "/Quiet", "/NoRestart" -Wait
+
+# Optionally clean up
+Remove-Item $localPath -Force
