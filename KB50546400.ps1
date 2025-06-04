9f330e241602585a0e9f7968a76f812f7171b47a
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2025-06-04 16:09:52 -0500

    Update KB50546400.ps1

--- KB50546400.ps1
+++ KB50546400.ps1
@@ -1,7 +1,7 @@
 # Set URL or network path to the .cab file
 $updateUrl = "https://github.com/ljjSpectrum/Scripts/blob/main/windows10.0-kb5059607-x64_e3d41d6b69501d1851fb1afd6562859151f539af.cab"
-$localPath = "C:\TEMP\KB5046400.cab"
-$localDir = "C:\TEMP
+$localPath = "C:\TEMP2\KB5046400.cab"
+$localDir = "C:\TEMP2
 
 # Ensure the directory exists
 if (-not (Test-Path $localDir)) 
